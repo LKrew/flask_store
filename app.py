f75c187a8fe085f18e0d55b320e0927e3526f1d0
luke_krewson <krewson.luke@gmail.com>
2022-09-07 13:44:45 -0500

    added posgresql to app

--- app.py
+++ app.py
@@ -5,18 +5,14 @@ from resources.user import UserRegister
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 from flask_jwt import JWT
-from db import db
+import os
 
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASW_URL','sqlite:///data.db')
 app.secret_key = 'luke'
 api = Api(app)
-
-@app.before_first_request
-def create_tables():
-    db.create_all()
 
 jwt = JWT(app, authenticate, identity)
 
@@ -27,7 +23,7 @@ api.add_resource(Store, '/store/<string:name>')
 api.add_resource(StoreList, '/stores')
 
 if __name__=='__main__':
-
+    from db import db
     db.init_app(app)
 
     app.run(port=5000, debug=True)
